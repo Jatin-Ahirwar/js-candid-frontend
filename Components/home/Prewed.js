9f5ea3e50a1d42bb37067a87c75de96e05b3b51b
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-01-26 01:36:55 +0530

    create operations started

--- Components/home/Prewed.js
+++ Components/home/Prewed.js
@@ -1,16 +1,40 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "@/Components/home/Prewed.css"
 import Link from 'next/link'
 import { useDispatch, useSelector } from 'react-redux'
 import { asyncaallprewedding } from '@/Store/Actions/PreweddingActions'
 
 const Prewed = () => {
+    const [bridename, setbridename] = useState("")
+    const [groomname, setgroomname] = useState("")
+    const [date, setdate] = useState("")
+    const [location, setlocation] = useState("")
+    const [country, setcountry] = useState("")
+    const [posterimage, setposterimage] = useState("")
+    const [teaser, setteaser] = useState("")
+    const [images, setimages] = useState([])
+
     const dispatch = useDispatch()
     const { preweddings } = useSelector((state)=>state.PreweddingReducer)
     useEffect(()=>{
         dispatch(asyncaallprewedding())
     } , [])
+
+    const SubmitHandler = (e)=>{
+        e.preventDefault()
+        const prewedding = {
+            bridename,
+            groomname,
+            date,
+            location,
+            country,
+            images,
+            posterimage,
+            teaser
+        }
+        console.log(prewedding)
+    }
 
     return <> 
     <div className='preweddingdiv'>
@@ -36,13 +60,100 @@ const Prewed = () => {
                         </div>    
                     </Link>
                 ))
-
                 :
                 <div className='notfounddiv'>
                     <h1>there is no prewedding's</h1>
                 </div>
             }
+            {/* <div id='createprewedding' className='preweddingproductdiv'> */}
+            <div id='createprewedding' >
+                <img src="../../stockimages/create.png" alt="" />
+                <h2>Create Prewedding</h2>
+            </div>
         </div>    
+
+        <div className='preweddingoverlay'>
+            <div className='preweddingformdiv'>
+            <h2 className='mandatory'>Create Prewedding</h2>
+            <form onSubmit={SubmitHandler} encType='multipart/form-data' className='form'>
+
+                <div className='bgdiv'>
+                    <div className='boxes'>
+                    <label id='biglabels' className='label'>Bride Name  <span> (required)</span></label>
+                        <input className='input' onChange={(e)=>setbridename(e.target.value)} required type="text" />
+                    </div> 
+
+                    <div className='boxes'>
+                    <label id='biglabels' className='label'>Groom Name  <span> (required)</span></label>
+                        <input className='input' onChange={(e)=>setgroomname(e.target.value)} required type="text" />
+                    </div>  
+                </div> 
+
+                <div className='input-wrapper'>
+                    <label className='label'>Location  <span> (required)</span></label>
+                    <input className='input'onChange={(e)=>setlocation(e.target.value)} required type="text" />
+                </div>
+
+                <div className='input-wrapper'>
+                    <label className='label'>Country <span> (required)</span></label>
+                    <input className='input' onChange={(e)=>setcountry(e.target.value)} required type="text" />
+                </div>
+
+                <div className='input-wrapper'>
+                    <label className='label'>Date <span> (required)</span></label>
+                    <input className='input' onChange={(e)=>setdate(e.target.value)} required type="text" />
+                </div>
+
+                <div className='files-wrapper'>
+                    <label className='label'>Teaser  <span> (required)</span></label>
+                    <input required onChange={(e)=>setteaser(e.target.value)} type="file" />
+                </div>
+                
+                <div className='files-wrapper'>
+                    <label className='label'>Poster Image  <span> (required)</span></label>
+                    <input onChange={(e)=>setposterimage(e.target.value)}  required type="file" />
+                </div>
+
+                <div   className='files-wrapper'>
+                    <label className='label'>Images  <span> (required)</span></label>
+                    <input  multiple required type="file" />
+                </div>
+
+                <button type='submit' className='submitbtn'><p>submit</p></button>       
+            </form>
+            
+        </div>
+                {/* <div className='formdiv'>
+                    <form encType='multipart/form-data'>
+                        <div></div>
+                        <label>Bridename</label>
+                        <input type="text" placeholder='Bride Name' />
+                        
+                        <label>Groomname</label>
+                        <input type="text" placeholder='Groom Name' />
+                        
+                        <label>Date</label>
+                        <input type="text" placeholder='Date' />
+                        
+                        <label>Location</label>
+                        <input type="text" placeholder='Eg . Jaipur' />
+                        
+                        <label>Country</label>
+                        <input type="text" placeholder='Eg . India' />
+                        
+                        <label>Teaser</label>
+                        <input type="file"/>
+                        
+                        <label>Poster Image</label>
+                        <input type="file"/>
+
+                        <label>Images</label>
+                        <input type="file" multiple/>
+
+                        <input type="submit" />
+                    </form>
+                </div> */}
+            </div>
     </div>
    </>
 
