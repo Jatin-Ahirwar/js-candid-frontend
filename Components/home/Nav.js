4f311e3b83675417bbac50748da94aa73b8bd1ad
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2024-01-29 23:56:54 +0530

    nav responsive work done

--- Components/home/Nav.js
+++ Components/home/Nav.js
@@ -4,38 +4,54 @@ import Link from "next/link"
 import React, { useEffect, useRef, useState } from 'react'
 
 const Nav = () => {
+    
+    const OpenNavHandler = (e)=>{
+      e.preventDefault()
+      document.querySelector(".links").style.display = "flex"
+      document.querySelector(".menuicon").style.display = "none"
+      document.querySelector(".closingicon").style.display = "initial"
+    }
+
+    const CloseNavHandler = (e)=>{
+      e.preventDefault()
+      document.querySelector(".links").style.display = "none"
+      document.querySelector(".menuicon").style.display = "initial"
+      document.querySelector(".closingicon").style.display = "none"
+    }
+
+    const linkClickHandler = (e) => {
+      e.preventDefault()
+      document.querySelector(".links").style.display = "none";
+    };
+
   return <>
     <div className='nav'>
     <Link style={{textDecoration:"none" , color:"black"}} href="/"><h1 className="logo">Js<span>candid</span></h1></Link>
       <div className="links">
-          <Link className="link" href="/Content/stories">Stories</Link>
-          <Link className="link" href="/Content/images">Images</Link>
-          <Link className="link" href="/Content/prewedding">PreWeddings</Link>
-          <Link className="link" href="/Content/trailer">Trailers</Link>
-          <Link className="link" href="/Content/contact">Contact</Link>
-          <Link className="link" href="/Content/aboutus">About us</Link>
+          <Link className="link" onClick={linkClickHandler} href="/Content/stories">Stories</Link>
+          <Link className="link" onClick={linkClickHandler} href="/Content/images">Images</Link>
+          <Link className="link" onClick={linkClickHandler} href="/Content/prewedding">PreWeddings</Link>
+          <Link className="link" onClick={linkClickHandler} href="/Content/trailer">Trailers</Link>
+          <Link className="link" onClick={linkClickHandler} href="/Content/contact">Contact</Link>
+          <Link className="link" onClick={linkClickHandler} href="/Content/aboutus">About us</Link>
           <div className='profilediv'>
               <div id="ptag"> 
                 <p id="pp">MORE</p>
-                <img id="ptagicon" src="../../stockimages/arrow-down-s-fill.png" alt="" />
+                <img id="ptagicon" src="../../stockimages/down-arrow.png" alt="" />
                 {/* <i id="ptagicon" class="ri-arrow-down-s-fill"></i> */}
               </div>
                 <div className="hidden-nav">
-                    <Link id="hiddenlink"  href="/Content/kids">baby & Kids </Link>
-                    {/* <Link id="hiddenlink"  href="/Content/maternity">Maternity</Link> */}
-                    <Link id="hiddenlink"  href="/Content/event">Events</Link>
-                    <Link id="hiddenlink"  href="/Content/fashion">Fashion & Commercial</Link>
-                    <Link id="hiddenlink"  href="/Content/signin">Sign in</Link>
+                    <Link id="hiddenlink"  onClick={linkClickHandler} href="/Content/kids">baby & Kids </Link>
+                    {/* <Link id="hiddenlink"  onClick={linkClickHandler} href="/Content/maternity">Maternity</Link> */}
+                    <Link id="hiddenlink"  onClick={linkClickHandler} href="/Content/event">Events</Link>
+                    <Link id="hiddenlink"  onClick={linkClickHandler} href="/Content/fashion">Fashion & Commercial</Link>
+                    <Link id="hiddenlink"  onClick={linkClickHandler} href="/Content/signin">Sign in</Link>
                 </div>
           </div>
 
-          {/* <i id="closeicon" class="ri-close-fill"></i> */}
-          
-          {/* <img className="closingicon" src="../../stockimages/close.png" alt="" />           */}
+          <img className="closingicon" onClick={CloseNavHandler}  id="closenavicon"  src="../../stockimages/creame close.png" alt="" />                    
       </div>
-
-          <img className="menuicon" id="opennavicon" src="../../stockimages/menu.png" alt="" />
-
+          <img className="menuicon" onClick={OpenNavHandler} id="opennavicon" src="../../stockimages/menu.png" alt="" />
     </div>
   </>
 }
